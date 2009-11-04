1b9b3c1a512331fce5b6264f7d93813bb3bd4359
Ishaaq Chandy <ishaaq@gmail.com>
2009-11-04 23:16:20 +1100

    Implemented ability to build tarball
    - using rake for now -- too bad sbt can't do this as easily, perhaps it
      never will.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -7,8 +7,6 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
     override def packageOptions =  ManifestAttributes(("Agent-Class", "dash.Agent")) :: super.packageOptions.toList
 
     val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
-    val nexusReleases = "Melbourne R&D Repository" at "http://mel1u114:8081/nexus/content/groups/central"
-    val nexusSnapshots = "Melbourne R&D Snapshots Repository" at "http://mel1u114:8081/nexus/content/groups/snapshots"
 
     def toolsJar = {
       // java.home may point to the jre home, we want the jvm home to get to tools.jar
@@ -26,22 +24,4 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
     val mina = "org.apache.mina" % "mina-core" % "2.0.0-RC1" intransitive
     val slf4j = "org.slf4j" % "slf4j-api" % "1.5.8" intransitive
     val slf4j_jdk14 = "org.slf4j" % "slf4j-jdk14" % "1.5.8" intransitive
-
-    def assemblyExclude(base: PathFinder) = base / "META-INF" ** "*"
-
-    def assemblyOutputPath = outputPath / assemblyJarName
-    def assemblyJarName = artifactID + ".jar"
-    def assemblyTemporaryPath = outputPath / "assembly-libs"
-    def assemblyClasspath = super.compileClasspath +++ mainResources
-    def assemblyExtraJars = mainDependencies.scalaLibrary
-    def assemblyPaths(tempDir: Path, classpath: PathFinder, extraJars: PathFinder, exclude: PathFinder => PathFinder) = {
-        val (libs, directories) = classpath.get.toList.partition(ClasspathUtilities.isArchive)
-        for(jar <- extraJars.get ++ libs) FileUtilities.unzip(jar, tempDir, log).left.foreach(error)
-        val base = (Path.lazyPathFinder(tempDir :: directories) ##)
-        (descendents(base, "*") --- exclude(base)).get
-    }
-
-    lazy val assembly = assemblyTask(assemblyTemporaryPath, assemblyClasspath, assemblyExtraJars, assemblyExclude) dependsOn(compile)
-    def assemblyTask(tempDir: Path, classpath: PathFinder, extraJars: PathFinder, exclude: PathFinder => PathFinder) =
-    packageTask(Path.lazyPathFinder(assemblyPaths(tempDir, classpath, extraJars, exclude)), assemblyOutputPath, packageOptions)
 }
